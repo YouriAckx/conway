f69ed74d85c2e58b2ea263c826ca9b58fca47e0b
Youri Ackx <youri@ackx.net>
2016-08-26 20:36:49 +0200

    Added samples

--- groovy/src/conway.groovy
+++ groovy/src/conway.groovy
@@ -11,20 +11,6 @@ See the LICENSE file and [http://www.gnu.org/licenses/].
 */
 
 
-// Basic grid
-Set basic1 = [[0, 0], [1, 0], [2, 0], [1, 1], [2, 1]] as Set
-//Another grid
-Set basic2 = [[2, 2], [0, 1], [1, 1], [1, 0]] as Set
-// Block
-Set block = [[0, 0], [1, 0], [0, 1], [1, 1]] as Set
-// Blinker
-Set blinker = [[1, 1], [2, 1], [3, 1]] as Set
-// Glider
-Set glider =  [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]] as Set
-// Die hard
-Set diehard = [[1, 2], [2, 2], [2, 3], [7, 1], [6, 3], [7, 3], [8, 3]] as Set
-
-
 // Load from file
 def file = new File(args[0])
 if (!file.exists()) {
