c7fd3e5c377a812c527d03772efed51c7eb0baed
Youri Ackx <youri@ackx.net>
2016-08-26 21:03:35 +0200

    javadoc

--- groovy/src/Storage.groovy
+++ groovy/src/Storage.groovy
@@ -1,5 +1,13 @@
+/**
+ * Persistence
+ */
 class Storage {
 
+    /**
+     * Load a game from file.
+     * @param f File to load
+     * @return The game as a Set of [x, y] living cells.
+     */
     public static Set load(File f) {
         def grid = [] as Set
         f.eachLine { line, y ->
